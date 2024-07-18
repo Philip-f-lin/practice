1dfef724675b4a07decd0d3c2b1d0574269893fb
linzhuofei <philip.f.lin@gmail.com>
2024-07-18 17:54:09 +0800

    Stream流的終結方法

---
+++ javase/src/main/java/com/example/mystream/StreamDemo9.java
@@ -0,0 +1,41 @@
+package com.example.mystream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.IntFunction;
+
+public class StreamDemo9 {
+    public static void main(String[] args) {
+        /*
+         void forEach(Consumer action) 遍歷
+         long count() 統計
+         toArray() 收集流中的數據，放到陣列中
+         */
+
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, "張無忌", "週芷若", "趙敏", "張強", "張三豐", "張翠山", "張良", "王二麻子", "謝廣坤");
+
+        // void forEach(Consumer action) 遍歷
+        list.stream().forEach(s -> System.out.println(s));
+
+        // long count() 統計
+        long count = list.stream().count();
+        System.out.println(count);
+
+        // toArray() 收集流中的數據，放到陣列中
+        Object[] arr = list.stream().toArray();
+        System.out.println(Arrays.toString(arr));
+
+        String[] arr1 = list.stream().toArray(new IntFunction<String[]>() {
+            @Override
+            public String[] apply(int value) {
+                return new String[value];
+            }
+        });
+        System.out.println(Arrays.toString(arr1));
+
+        String[] arr2 = list.stream().toArray(value -> new String[value]);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+}
