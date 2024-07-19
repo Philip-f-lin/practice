3e690d19ee0cc0c25b7f202c0b9945570ce968a3
linzhuofei <philip.f.lin@gmail.com>
2024-07-19 09:31:35 +0800

    收集方法collect超詳解

--- javase/src/main/java/com/example/mystream/StreamDemo9.java
+++ javase/src/main/java/com/example/mystream/StreamDemo9.java
@@ -1,41 +1,41 @@
 package com.example.mystream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.function.IntFunction;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StreamDemo9 {
     public static void main(String[] args) {
         /*
-         void forEach(Consumer action) 遍歷
-         long count() 統計
-         toArray() 收集流中的數據，放到陣列中
+         collect(Collector collector) 收集流中的數據，放到集合中 (List Set Map)
+
+         注意點：
+         如果我們要收集到Map集合當中，鍵就不能重複，否則會報錯
          */
 
         ArrayList<String> list = new ArrayList<>();
-        Collections.addAll(list, "張無忌", "週芷若", "趙敏", "張強", "張三豐", "張翠山", "張良", "王二麻子", "謝廣坤");
+        Collections.addAll(list, "張無忌-男-15", "周芷若-女-14", "趙敏-女-13", "張強-男-20",
+                "張三豐-男-100", "張翠山-男-40", "張良-男-35", "王二麻子-男-37", "謝廣坤-男-41");
 
-        // void forEach(Consumer action) 遍歷
-        list.stream().forEach(s -> System.out.println(s));
 
-        // long count() 統計
-        long count = list.stream().count();
-        System.out.println(count);
+        //收集List集合當中
+        //需求：
+        //我要把所有的男性收集起來
+        List<String> newList1 = list.stream()
+                .filter(s -> "男".equals(s.split("-")[1]))
+                .collect(Collectors.toList());
+        System.out.println(newList1);
 
-        // toArray() 收集流中的數據，放到陣列中
-        Object[] arr = list.stream().toArray();
-        System.out.println(Arrays.toString(arr));
+        //收集Set集合當中
+        //需求：
+        //我要把所有的男性收集起來
+        Set<String> newList2 = list.stream().filter(s -> "男".equals(s.split("-")[1]))
+                .collect(Collectors.toSet());
+        System.out.println(newList2);
 
-        String[] arr1 = list.stream().toArray(new IntFunction<String[]>() {
-            @Override
-            public String[] apply(int value) {
-                return new String[value];
-            }
-        });
-        System.out.println(Arrays.toString(arr1));
 
-        String[] arr2 = list.stream().toArray(value -> new String[value]);
-        System.out.println(Arrays.toString(arr2));
+
 
     }
 }
