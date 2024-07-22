bddb8464db3a03acfe7c695551d4a02de5fbd44b
linzhuofei <philip.f.lin@gmail.com>
2024-07-22 14:40:07 +0800

    Servlet執行流程&生命週期

---
+++ javaweb/src/main/java/com/philip/web/ServletDemo2.java
@@ -0,0 +1,38 @@
+package com.philip.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/demo2", loadOnStartup = 1)
+public class ServletDemo2 implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init...");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("servlet hello world ~");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destory...");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+
+}
