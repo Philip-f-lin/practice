e934d11fb8e88cace5761ae7d39bcd041f8ae74a
linzhuofei <philip.f.lin@gmail.com>
2024-07-31 15:03:10 +0800

    Session - 基本使用

---
+++ javaweb/src/main/java/com/philip/web/session/SessionDemo2.java
@@ -0,0 +1,28 @@
+package com.philip.web.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/sessionDemo2")
+public class SessionDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 獲取數據，從 Session 中
+
+        //1. 獲取 Session 對象
+        HttpSession session = req.getSession();
+        //2. 獲取數據
+        Object username = session.getAttribute("username");
+        System.out.println(username);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
