c4cb4681d100dbde60c15fc7b6b82d08cb154bd0
linzhuofei <philip.f.lin@gmail.com>
2024-08-06 14:57:17 +0800

    多線程 & JUC - 同步代碼塊

--- javase/src/main/java/com/example/mythread/a09threadsafe1/MyThread.java
+++ javase/src/main/java/com/example/mythread/a09threadsafe1/MyThread.java
@@ -5,19 +5,25 @@ public class MyThread extends Thread{
     //表示這個類別所有的對象，都共享ticket數據
     static int ticket = 0; // 0 ~ 99
 
+    // 鎖物件，一定要是唯一的
+    static Object object = new Object();
+
     @Override
     public void run() {
         while (true){
-            if (ticket < 100){
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            // 同步代碼塊
+            synchronized (object){
+                if (ticket < 1000){
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    ticket ++;
+                    System.out.println(getName() + "正在賣第" + ticket + "張票" );
+                }else {
+                    break;
                 }
-                ticket ++;
-                System.out.println(getName() + "正在賣第" + ticket + "張票" );
-            }else {
-                break;
             }
         }
     }
