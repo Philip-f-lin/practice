bf9196e50df7271d884004e6ffd361750ad3f1fc
linzhuofei <philip.f.lin@gmail.com>
2024-08-09 15:05:42 +0800

    CompletableFuture 之 get 容易阻塞

---
+++ JUC/src/main/java/demo1/FutureAPIDemo.java
@@ -0,0 +1,34 @@
+package demo1;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class FutureAPIDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        /**
+         *1 get容易導致堵塞，一般建議成在程式後面，一旦呼叫不見不散，非要等到結果才會離開，不管你是否計算完成，容易程式堵塞。
+         *2 假如不願意等待，希望過時不候，可以自動離開
+         */
+
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t ----come in");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "task over";
+        });
+        Thread t1 = new Thread(futureTask, "t1");
+        t1.start();
+
+        System.out.println(Thread.currentThread().getName() + "\t ---忙其他任務");
+
+        //System.out.println(futureTask.get()); // get容易導致堵塞
+        System.out.println(futureTask.get(3, TimeUnit.SECONDS)); // 假如不願意等待，希望過時不候，可以自動離開
+    }
+
+
+}
