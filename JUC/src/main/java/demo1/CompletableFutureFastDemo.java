24123a4ce6e07c2a581f4bd6edba6e7f11d1e023
linzhuofei <philip.f.lin@gmail.com>
2024-08-14 14:16:07 +0800

    CompletableFuture 之對計算速度選用

---
+++ JUC/src/main/java/demo1/CompletableFutureFastDemo.java
@@ -0,0 +1,34 @@
+package demo1;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureFastDemo {
+    public static void main(String[] args) {
+        CompletableFuture<String> playA = CompletableFuture.supplyAsync(() -> {
+            System.out.println("A come in");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "playA";
+        });
+
+        CompletableFuture<String> playB = CompletableFuture.supplyAsync(() -> {
+            System.out.println("B come in");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "playB";
+        });
+
+        CompletableFuture<String> result = playA.applyToEither(playB, f -> {
+            return "winner is: " + f;
+        });
+
+        System.out.println(Thread.currentThread().getName() + "\t" + result.join());
+    }
+}
