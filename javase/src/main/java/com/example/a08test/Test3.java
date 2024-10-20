ba689e01c29ec79e6d5a895d523d9a31428c2525
linzhuofei <philip.f.lin@gmail.com>
2024-10-20 23:46:15 +0800

    集合進階 - 綜合練習3 - 不重複的隨機點名

---
+++ javase/src/main/java/com/example/a08test/Test3.java
@@ -0,0 +1,12 @@
+package com.example.a08test;
+
+public class Test3 {
+    public static void main(String[] args) {
+        /*
+        班上有5個學生
+        要求：
+        被點到的學生不會再被點到。
+        但如果班上所有的學生都點完了，就需要重新開始第二輪點名
+        */
+    }
+}
